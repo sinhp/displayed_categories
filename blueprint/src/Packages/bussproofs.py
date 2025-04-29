d6dbe810ca34a41d52de9498915fc9a2013878f8
Sina Hazratpour <sinahazratpour@gmail.com>
2025-04-29 18:29:47 +0200

    def display

--- blueprint/src/Packages/bussproofs.py
+++
@@ -1,7 +0,0 @@
-from plasTeX import VerbatimEnvironment
-
-def ProcessOptions(options, document):
-    document.config['images']['scales'].setdefault('prooftree', 1.5)
-
-class prooftree(VerbatimEnvironment):
-    pass
